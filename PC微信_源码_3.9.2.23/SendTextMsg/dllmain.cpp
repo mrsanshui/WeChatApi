8c3c3f952a42208c63dac6cbb9c668f1bfdb5efc
三水君 <437382693@qq.com>
2023-09-10 22:28:24 +0800

    WeChatApi V-1.2.0 1、新增3.9.2.23源码 2、新增3.9.5.81源码 3、优化文档

---
+++ PC微信_源码_3.9.2.23/SendTextMsg/dllmain.cpp
@@ -0,0 +1,78 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+
+class WxStruct
+{
+public:
+	wchar_t* pStr;  //文本数据的指针
+	int minLen;     //字符串的长度
+	int maxLen;     //字符串的最大长度
+	int full1;
+	int full2;
+public:
+	WxStruct(wchar_t* _pStr)
+	{
+		pStr = _pStr;
+		minLen = wcslen(pStr);
+		maxLen = minLen * 2;
+		full1 = 0;
+		full2 = 0;
+	}
+};
+
+void Hook();
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+		HANDLE Thandle = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hook, NULL, NULL, NULL);
+		if (Thandle != 0)
+		{
+			CloseHandle(Thandle);
+		}
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+void Hook()
+{
+	//模块基址
+	DWORD baseAddr = (DWORD)LoadLibraryA("WeChatWin.dll");
+	DWORD callAddr = baseAddr + 0xCE6C80;
+
+	//构造数据
+	WxStruct sWxid((wchar_t*)L"filehelper");
+	WxStruct sMsg((wchar_t*)L"我是测试消息");
+	
+	//缓存区
+	char buf1[0x10] = { 0 };
+	char buf2[0x1000] = { 0 };
+
+	__asm
+	{
+		push 0x0;
+		push 0x0;
+		lea eax, buf1;
+		push 0x0;
+		push 0x1;
+		push eax;
+		lea eax, sMsg;
+		push eax;
+		lea edx, sWxid;
+		lea ecx, buf2;
+		call callAddr;
+		add esp, 0x18;
+	}
+}
---
+++ PC微信_源码_x64_3.9.5.81/SendImgMsg/dllmain.cpp
@@ -0,0 +1,92 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <string>
+using namespace std;
+
+class WxStruct
+{
+public:
+	wchar_t* pStr;  //文本数据的指针
+	DWORD strLen;   //字符串的长度【低位】
+	DWORD maxLen;   //字符串的最大长度【高位】
+	DWORD64 full1;
+	DWORD full2;
+public:
+	WxStruct(wchar_t* _pStr)
+	{
+		pStr = _pStr;
+		strLen = wcslen(pStr);
+		maxLen = strLen * 2;
+		full1 = 0;
+		full2 = 0;
+	}
+};
+
+void Hook();
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		HANDLE Thandle = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hook, NULL, NULL, NULL);
+		if (Thandle != 0)
+		{
+			CloseHandle(Thandle);
+		}
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+void Hook()
+{
+	//模块基址
+	DWORD64 baseAddr = (DWORD64)LoadLibraryA("WeChatWin.dll");
+	DWORD64 callAddr1 = baseAddr + 0x8C00E0;
+	DWORD64 callAddr2 = baseAddr + 0xFCCF10;
+
+	//构造函数指针
+	typedef DWORD64(*Call1)();
+	typedef DWORD64(*Call2)(DWORD64, DWORD64, DWORD64, DWORD64, DWORD64);
+
+	//构造数据
+	WxStruct sWxid((wchar_t*)L"filehelper");
+	WxStruct imagefilepath((wchar_t*)L"C:\\Users\\Administrator\\Desktop\\1.png");
+
+	//缓冲区
+	char buf1[0x450] = { 0 };
+	char buf2[0x450] = { 0 };
+
+	//指针数组
+	DWORD64* flag[10] = { 0 };
+	DWORD64 temp1 = 0;
+	DWORD64 temp2 = 0;
+	flag[1] = (DWORD64*)&buf2;
+	flag[8] = &temp1;
+	flag[9] = &temp2;
+
+	//获取句柄
+	Call1 GetHandle = (Call1)callAddr1;
+	DWORD64 h = GetHandle();
+
+	//发送图片消息
+	Call2 SendImg = (Call2)callAddr2;
+	SendImg(
+		h,
+		(DWORD64)&buf1,
+		(DWORD64)&sWxid,
+		(DWORD64)&imagefilepath,
+		(DWORD64)&flag
+	);
+
+	OutputDebugStringA("OK");
+}
