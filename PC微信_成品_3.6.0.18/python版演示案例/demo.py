19be74d773ec99fa793fcca7132494d1397cc793
三水君 <437382693@qq.com>
2024-02-19 22:33:53 +0800

    WeChatApi V-1.3.2 1、优化demo：启动前先校验微信版本是否正确

--- PC微信_成品_3.6.0.18/python版演示案例/demo.py
+++ PC微信_成品_3.6.0.18/python版演示案例/demo.py
@@ -4,6 +4,28 @@ from threading import Thread
 import requests
 import json
 import time
+import winreg
+import sys
+
+
+def get_wx_version():
+    """获取微信版本号"""
+
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Tencent\WeChat", 0, winreg.KEY_READ) as key:
+            int_version = winreg.QueryValueEx(key, "Version")[0]
+            hex_version = hex(int_version)
+            hex_str = hex_version[2:]
+            new_hex_str = "0" + hex_str[1:]
+            new_hex_num = int(new_hex_str, 16)
+            major = (new_hex_num >> 24) & 0xFF
+            minor = (new_hex_num >> 16) & 0xFF
+            patch = (new_hex_num >> 8) & 0xFF
+            build = (new_hex_num >> 0) & 0xFF
+            return "{}.{}.{}.{}".format(major, minor, patch, build)
+    except Exception as e:
+        print("打开注册表失败：{}".format(e))
+        return None
 
 
 class HttpServer(BaseHTTPRequestHandler):
@@ -25,6 +47,12 @@ if __name__ == '__main__':
     # 注意！！！请确保你的微信版本是【3.6.0.18】
     # 注意！！！请确保你的微信版本是【3.6.0.18】
     # 注意！！！请确保你的微信版本是【3.6.0.18】
+
+    # 校验微信版本号是否正确
+    wx_version = get_wx_version()
+    if wx_version != "3.6.0.18":
+        print("当前微信版本为：【{}】，请确保你的微信版本是【3.6.0.18】".format(wx_version))
+        sys.exit(0)
 
     dll_port = 8748  # dll的端口号
     my_port = 9001   # 自己的端口号
