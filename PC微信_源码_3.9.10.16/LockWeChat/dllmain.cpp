db12c9570037aa6c735f73323ecfdcebbb74cf09
三水君 <437382693@qq.com>
2024-05-08 04:22:07 +0800

    WeChatApi V-1.4.0 1、新增3.9.10.16源码

---
+++ PC微信_源码_3.9.10.16/LockWeChat/dllmain.cpp
@@ -0,0 +1,37 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+void LockWeChat();
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        LockWeChat();
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+void LockWeChat()
+{
+    DWORD64 baseAddr = (DWORD64)LoadLibraryA("WeChatWin.dll");
+	DWORD64 callAddr1 = baseAddr + 0x13322B0;
+	DWORD64 callAddr2 = baseAddr + 0x12FC5A0;
+    __asm
+    {
+        call callAddr1;
+        mov ecx, eax;
+        call callAddr2;
+    }
+    OutputDebugStringA("OK");
+}
